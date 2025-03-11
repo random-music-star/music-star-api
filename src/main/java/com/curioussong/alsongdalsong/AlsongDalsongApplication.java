2ab3848cc5f240c5ea4e4f63779f68e6aaa08b68
donggyu <use02012@gmail.com>
2025-03-11 18:04:41 +0900

    init: Spring Boot 프로젝트 생성
    - 웹 애플리케이션 기본 설정 추가
    - Spring Web 의존성 추가
    - 기본 패키지 구조 설정

---
+++ src/main/java/com/curioussong/alsongdalsong/AlsongDalsongApplication.java
@@ -0,0 +1,13 @@
+package com.curioussong.alsongdalsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlsongDalsongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlsongDalsongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/curioussong/alsongdalsong/AlsongDalsongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curioussong.alsongdalsong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlsongDalsongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
