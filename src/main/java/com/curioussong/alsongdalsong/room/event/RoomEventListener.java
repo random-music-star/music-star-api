0cb2cef1b06a9347e248b331480551d923b1d227
이동규 <use02012@gmail.com>
2025-03-31 19:05:27 +0900

    refactor: 방 정보 sse 전송 시 년도 정보 추가

--- src/main/java/com/curioussong/alsongdalsong/room/event/RoomEventListener.java
+++ src/main/java/com/curioussong/alsongdalsong/room/event/RoomEventListener.java
@@ -4,6 +4,8 @@ import com.curioussong.alsongdalsong.common.sse.SseEmitterManager;
 import com.curioussong.alsongdalsong.game.domain.GameMode;
 import com.curioussong.alsongdalsong.room.dto.RoomDTO;
 import com.curioussong.alsongdalsong.roomgame.repository.RoomGameRepository;
+import com.curioussong.alsongdalsong.roomyear.domain.RoomYear;
+import com.curioussong.alsongdalsong.roomyear.repository.RoomYearRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -21,6 +23,7 @@ public class RoomEventListener {
 
     private final SseEmitterManager sseEmitterManager;
     private final RoomGameRepository roomGameRepository;
+    private final RoomYearRepository roomYearRepository;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleRoomUpdatedEvent(RoomUpdatedEvent event) {
@@ -33,6 +36,10 @@ public class RoomEventListener {
             RoomDTO roomDTO = event.room().toDto();
             List<GameMode> gameModes = roomGameRepository.findGameModesByRoomId(roomDTO.getId());
             roomDTO.setGameModes(gameModes);
+            List<Integer> years = roomYearRepository.findAllByRoom(event.room()).stream()
+                    .map(RoomYear::getYear)
+                    .toList();
+            roomDTO.setYears(years);
             data.put("room", roomDTO);
         } else {
             log.info("방 종료 이벤트 발생");
