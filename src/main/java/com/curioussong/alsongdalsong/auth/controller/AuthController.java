7aa13a6619e9bb0fdcbbf026ab72dd3861f77fbe
leedongkyu0407 <use02012@gmail.com>
2025-04-24 21:22:06 +0900

    feat: username 반환 endpoint 구현

--- src/main/java/com/curioussong/alsongdalsong/auth/controller/AuthController.java
+++ src/main/java/com/curioussong/alsongdalsong/auth/controller/AuthController.java
@@ -1,11 +1,15 @@
 package com.curioussong.alsongdalsong.auth.controller;
 
 import com.curioussong.alsongdalsong.auth.dto.UserSignupRequest;
+import com.curioussong.alsongdalsong.auth.dto.UsernameResponse;
 import com.curioussong.alsongdalsong.auth.service.AuthService;
+import com.curioussong.alsongdalsong.game.dto.userinfo.UserInfoResponse;
 import com.curioussong.alsongdalsong.jwt.JwtTokenProvider;
 import com.curioussong.alsongdalsong.jwt.dto.TokenResponse;
 import com.curioussong.alsongdalsong.auth.dto.UserLoginRequest;
 import com.curioussong.alsongdalsong.auth.dto.UserLoginResponse;
+import com.curioussong.alsongdalsong.member.domain.Member;
+import com.curioussong.alsongdalsong.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +24,7 @@ import java.util.List;
 public class AuthController {
     private final AuthService authService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final MemberService memberService;
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signup(@RequestBody UserSignupRequest request) {
@@ -39,5 +44,12 @@ public class AuthController {
         String guestToken = authService.guestLogin();
         String token = jwtTokenProvider.createToken(guestToken);
         return ResponseEntity.ok(new TokenResponse(token));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getUsername(){
+        Member member = memberService.getCurrentMember();
+        UsernameResponse usernameResponse = new UsernameResponse(member.getUsername());
+        return ResponseEntity.ok(usernameResponse);
     }
 }
---
+++ src/main/java/com/curioussong/alsongdalsong/auth/dto/UsernameResponse.java
@@ -0,0 +1,4 @@
+package com.curioussong.alsongdalsong.auth.dto;
+
+public record  UsernameResponse (String username){
+}
