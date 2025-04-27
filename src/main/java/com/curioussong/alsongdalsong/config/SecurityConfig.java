388f7c1decf91dec89eac4a7a7faf0aae3f23f61
aseongjun99 <154566676+aseongjun99@users.noreply.github.com>
2025-04-27 23:36:14 +0900

    feat: permit 목록에 wss 추가

--- src/main/java/com/curioussong/alsongdalsong/config/SecurityConfig.java
+++ src/main/java/com/curioussong/alsongdalsong/config/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig {
                                 "/error",
                                 "/sse/**",
                                 "/channel",
-                                "/ws/**")
+                                "/ws/**",
+                                "/wss/**")
                         .permitAll()
                         .anyRequest().authenticated()
                 )
